4635f07fc52ee12e645c83364f295c5879933110
Samuel Patmore <spatmore@ford.com>
2020-03-09 13:04:00 -0700

    Combine filtering objects into FilterResult

---
+++ app/src/main/java/com/daydreamapplications/livedataextensions/result/FilterResult.kt
@@ -0,0 +1,53 @@
+package com.daydreamapplications.livedataextensions.result
+
+import androidx.lifecycle.LiveData
+import com.daydreamapplications.livedataextensions.filter
+import com.daydreamapplications.livedataextensions.map
+
+object FilterResult {
+
+    /**
+     * Return a LiveData instance that emits values emitted by the source LiveData only if the value is Error
+     */
+    fun <S> filterError(source: LiveData<Result<S>>): LiveData<Throwable> {
+        return source.filter { it is Result.Error }
+            .map { (it as Result.Error).exception }
+    }
+
+    /**
+     * Return a LiveData instance that emits values emitted by the source LiveData only if the value is Loading
+     */
+    fun <S> filterLoading(source: LiveData<Result<S>>): LiveData<Result.Loading> {
+        return source.filter { it is Result.Loading }
+            .map { it as Result.Loading }
+    }
+
+    /**
+     * Return a LiveData instance that emits values emitted by the source LiveData only if the value is Success
+     */
+    fun <S> filterSuccess(source: LiveData<Result<S>>): LiveData<S> {
+        return source.filter { it is Result.Success }
+            .map { (it as Result.Success).data }
+    }
+}
+
+/**
+ * Return LiveData that emits only Error values emitted by the source LiveData
+ */
+fun <T> LiveData<Result<T>>.filterError(): LiveData<Throwable> {
+    return FilterResult.filterError(this)
+}
+
+/**
+ * Return LiveData that emits only Loading values emitted by the source LiveData
+ */
+fun <T> LiveData<Result<T>>.filterLoading(): LiveData<Result.Loading> {
+    return FilterResult.filterLoading(this)
+}
+
+/**
+ * Return LiveData that emits only Success values emitted by the source LiveData
+ */
+fun <T> LiveData<Result<T>>.filterSuccess(): LiveData<T> {
+    return FilterResult.filterSuccess(this)
+}
