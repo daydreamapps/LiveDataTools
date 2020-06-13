79355996a8fb533c2e1fdc74344ec2af9e3a6583
Samuel Patmore <daydreamapplications@gmail.com>
2020-06-13 16:49:13 +0100

    Extract & test Take

--- app/src/main/java/com/daydreamapplications/livedataextensions/livedata/Filter.kt
+++ app/src/main/java/com/daydreamapplications/livedataextensions/livedata/Filter.kt
@@ -28,16 +28,7 @@ object Filter {
      * Filters items emitted by a LiveData by only emitting non-null values which satisfy a specified predicate
      */
     fun <T> filterNonNull(source: LiveData<T>, predicate: (T) -> Boolean): LiveData<T> {
-
-        val mediator = MediatorLiveData<T>()
-
-        mediator.addSource(source) { value ->
-            if (value != null && predicate(value)) {
-                mediator.value = value
-            }
-        }
-
-        return mediator
+        return filter(source) { it != null && predicate(it) }
     }
 }
 
